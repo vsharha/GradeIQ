ed4ef4a392beaf2096c7342503c43ec1dd5e854c
Viktor Sharha <vsharha07@gmail.com>
2025-10-09 20:30:48 +0100

    added cursor-pointer

--- src/components/assignments/Assignment.jsx
+++ src/components/assignments/Assignment.jsx
@@ -20,8 +20,8 @@ function Assignment({assignment, index, onClick}) {
   }
 
   return (
-    <div className="w-full p-2 min-h-64 sm:w-1/2 md:w-1/3 lg:w-1/4 select-none" onClick={handleClick}>
-      <Card className="h-full overflow-hidden hover:-translate-y-2 transition-all duration-150 hover:shadow-lg shadow-shadow/50 relative">
+    <div className="w-full p-2 min-h-64 sm:w-1/2 md:w-1/3 lg:w-1/4 select-none">
+      <Card className="h-full overflow-hidden hover:-translate-y-2 transition-all duration-150 hover:shadow-lg shadow-shadow/50 relative cursor-pointer" onClick={handleClick}>
         <OverlayLoader isLoading={isLoading}/>
         <CardHeader>
           <h2 className="font-bold text-lg">{title}</h2>
