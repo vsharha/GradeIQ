30389608223559bf8885d631200d8e7f0fa825b8
Viktor Sharha <vsharha07@gmail.com>
2025-10-09 01:57:38 +0100

    improve homepage

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,13 @@
 import { ModeToggle } from "@/components/ui/mode-toggle";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div></div>
+    <main className="flex items-center justify center flex-col gap-3 mt-20">
+      <h1 className="text-3xl w-fit font-bold">GradeIQ</h1>
+      <h2 className="w-fit text-xl">The future of teaching, now</h2>
+      <Link href={"/app"}><Button className="text-lg">Try it</Button></Link>
+    </main>
   );
 }
