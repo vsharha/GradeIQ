8f9316ea840b3c24c2c510879d556196a758abfa
Viktor Sharha <vsharha07@gmail.com>
2025-10-10 01:21:06 +0100

    add chevron

--- src/components/assignments/Assignment.jsx
+++ src/components/assignments/Assignment.jsx
@@ -5,7 +5,7 @@ import {Card, CardContent, CardFooter, CardHeader} from "@/components/ui/card";
 import {useState, useEffect} from "react";
 import OverlayLoader from "@/components/custom/OverlayLoader";
 import { formatDate } from "@/services/services";
-import { Calendar } from "lucide-react";
+import { Calendar, ChevronRight } from "lucide-react";
 
 function Assignment({assignment, index, onClick}) {
   const { title, createdOn, description, submitted, marked, dueDate } = assignment
@@ -26,16 +26,17 @@ function Assignment({assignment, index, onClick}) {
       <Card className="h-full overflow-hidden hover:-translate-y-2 transition-all duration-150 hover:shadow-lg shadow-shadow/50 relative cursor-pointer" onClick={handleClick}>
         <OverlayLoader isLoading={isLoading}/>
         <CardHeader>
-          <h2 className="font-bold text-lg">{title}</h2>
+          <div className="font-bold text-lg flex flex-row justify-between">
+            <span className="flex-1">
+              <h2>{title}</h2>
+            </span>
+            <ChevronRight size={24} className="translate-y-1"/>
+          </div>
           <div className="flex gap-3 flex-wrap justify-between">
             <div className="flex gap-2 flex-row items-center">
               <span className="flex items-center gap-2"><Calendar size={15}/> Due:</span>
               <p className="text-sm text-muted-foreground">{formatDate(dueDate)}</p>
             </div>
-            {/*<div>*/}
-            {/*  <span className="flex items-center gap-2"><Calendar size={15}/> Created On:</span>*/}
-            {/*  <h3 className="text-sm text-muted-foreground">{formatDate(createdOn)}</h3>*/}
-            {/*</div>*/}
           </div>
         </CardHeader>
         <CardContent className="flex-1">
