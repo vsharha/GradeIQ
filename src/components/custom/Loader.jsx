0ec0ea85d7ee0b41d945d1897a3366ff672e04c9
Viktor Sharha <vsharha07@gmail.com>
2025-10-11 22:39:01 +0100

    improve loader colors

--- src/components/custom/Loader.jsx
+++ src/components/custom/Loader.jsx
@@ -1,7 +1,7 @@
 function Loader({size = 1}) {
   return (
     <div className="overflow-hidden">
-      <div style={{height:`${2*size}rem`, width:`${2*size}rem`, borderWidth:`${0.25*size}rem`}} className="border-input border-t-muted-foreground rounded-full animate-spin"></div>
+      <div style={{height:`${2*size}rem`, width:`${2*size}rem`, borderWidth:`${0.25*size}rem`}} className="border-t-stone-500 border-stone-400 rounded-full animate-spin dark:border-input dark:border-t-muted-foreground"></div>
     </div>
   );
 }
