09bf249bc276e590a25d600f0999f82e6fe4d33c
Viktor Sharha <vsharha07@gmail.com>
2025-10-12 19:26:34 +0100

    update loader logic

--- src/components/custom/OverlayLoader.jsx
+++ src/components/custom/OverlayLoader.jsx
@@ -1,9 +1,11 @@
-import { cn } from "@/lib/utils";
 import Loader from "@/components/custom/Loader";
 
 function OverlayLoader({isLoading}) {
+  if(!isLoading)
+    return null;
+
   return (
-    <div className={cn("absolute top-0 w-full h-full flex justify-center items-center backdrop-blur brightness-95 z-45 transition-all duration-300", isLoading?"opacity-100":"opacity-0")}><Loader/></div>
+    <div className="absolute top-0 w-full h-full flex justify-center items-center backdrop-blur brightness-95 z-45 animate-fade-in-loader"><Loader/></div>
   );
 }
 
