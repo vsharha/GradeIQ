b44287033ed46d0cd293c0c04e1088ced20a062a
Viktor Sharha <vsharha07@gmail.com>
2025-10-13 20:36:38 +0100

    adjust progress error display

--- src/components/assignments/StyledProgress.jsx
+++ src/components/assignments/StyledProgress.jsx
@@ -1,15 +1,17 @@
 import { Progress } from "@/components/ui/progress";
-import { Loader } from "lucide-react";
+import { LucideX } from "lucide-react";
 import AnimatedLoader from "@/components/loader/AnimatedLoader";
 
-function StyledProgress({value, max, label, isLoading, ...props}) {
+function StyledProgress({value, max, label, isLoading, error, ...props}) {
   const percentageValue = value/max * 100
 
   return (
     <div className="w-full flex flex-col gap-2">
       <div className="flex gap-3 justify-between">
         <span>{label}</span>
-        {isLoading?<AnimatedLoader size={20}/>:<span>{value}/{max}</span>}
+        {isLoading && <AnimatedLoader size={20}/>}
+        {error && <span><LucideX/> Error</span>}
+        {!error && !isLoading && <span>{value}/{max}</span>}
       </div>
       <Progress value={percentageValue} {...props}/>
     </div>
--- src/components/custom/SubmissionProgress.jsx
+++ src/components/custom/SubmissionProgress.jsx
@@ -1,21 +1,16 @@
 import StyledProgress from "@/components/assignments/StyledProgress";
 import useSubmissions from "@/hooks/useSubmissions";
 import ErrorMessage from "@/components/custom/ErrorMessage";
-import BlockLoader from "@/components/loader/BlockLoader";
 
 function SubmissionProgress({ assignment }) {
   const { id } = assignment;
 
   const { submissions, error, isLoading } = useSubmissions(id)
 
-  if (error) {
-    return <ErrorMessage error={error} />
-  }
-
   const confirmed_count = submissions?.reduce((total, submission) => total + (submission.grade_confirmed ? 1 : 0), 0)
 
   return (
-    <StyledProgress value={confirmed_count || 0} max={submissions?.length || 0} label="Grading progress" isLoading={isLoading}/>
+    <StyledProgress value={confirmed_count || 0} max={submissions?.length || 0} label="Grading progress" isLoading={isLoading} error={error}/>
   );
 }
 
