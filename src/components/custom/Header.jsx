cf0736bfb5b3509e24f53e2cc3eda804b8b62464
Viktor Sharha <vsharha07@gmail.com>
2025-10-13 15:14:39 +0100

    changed header to use justify end

--- src/components/custom/Header.jsx
+++ src/components/custom/Header.jsx
@@ -12,7 +12,7 @@ function Header({showSettings=true, children}) {
             <Logo/>
           </Link>
         </div>
-        <div className="flex-1 flex flex-row px-5">
+        <div className="flex-1 flex flex-row px-5 justify-end">
           {children}
         </div>
         <div className="flex items-center gap-3">
