72ffae416919dc1e812d4e85b93d6c3c8d3567fd
Viktor Sharha <vsharha07@gmail.com>
2025-10-18 01:48:03 +0100

    prevent pdf viewer flickering

--- src/components/custom/PdfViewer.jsx
+++ src/components/custom/PdfViewer.jsx
@@ -6,14 +6,13 @@ import { useState } from "react";
 pdfjs.GlobalWorkerOptions.workerSrc = "/pdf.worker.min.mjs";
 
 function PdfViewer({url, setNumPages, pageNumber, setPageNumber}) {
-  const [width, setWidth] = useState(100);
-  const [height, setHeight] = useState(200);
+  const [dimensions, setDimensions] = useState({ width: null, height: null });
 
   const scale = 1
 
   return (
     <div
-      className="overflow-auto w-full"
+      style={{overflow:'auto', width:'full'}}
     >
       <Document
         file={url}
@@ -21,24 +20,29 @@ function PdfViewer({url, setNumPages, pageNumber, setPageNumber}) {
           setNumPages(numPages);
           setPageNumber((p) => Math.min(Math.max(1, p), numPages));
         }}
-        loading={<p>Loading PDF …</p>}
         error={<p>Failed to load PDF.</p>}
-        className="h-fit w-fit bg-white text-black"
-        style={{ height, width }}
       >
-        <Page
-          key={pageNumber}
-          pageNumber={pageNumber}
-          scale={scale}
-          onLoadSuccess={(page) => {
-            const viewport = page.getViewport({ scale });
-            setHeight(viewport.height);
-            setWidth(viewport.width)
-          }}
-        />
+        <div
+          style={
+            dimensions.width && dimensions.height
+              ? { minHeight: dimensions.height, minWidth: dimensions.width, display: 'flex', alignItems: 'center', justifyContent: 'center' }
+              : {}
+          }
+        >
+          <Page
+            key={pageNumber}
+            pageNumber={pageNumber}
+            scale={scale}
+            onLoadSuccess={(page) => {
+              const viewport = page.getViewport({ scale });
+              setDimensions({ height: viewport.height, width: viewport.width });
+            }}
+            loading={<div style={{ height: dimensions.height || 'auto', width: dimensions.width || 'auto', background: 'white', text: 'black' }} />}
+          />
+        </div>
       </Document>
     </div>
   );
 }
 
-export default PdfViewer;
+export default PdfViewer
