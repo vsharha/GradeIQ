3175465e6c1fecaaa8ee3f001d59fe468e25d8ae
Viktor Sharha <vsharha07@gmail.com>
2025-10-18 22:29:33 +0100

    comment out prefetching logic for now

--- src/app/app/assignments/layout.jsx
+++ src/app/app/assignments/layout.jsx
@@ -1,22 +1,22 @@
-import { HydrationBoundary, QueryClient, dehydrate } from "@tanstack/react-query";
-import getServerAuthHeaders from "@/services/getServerAuthHeaders";
-import { fetchAssignments } from "@/services/fetchApi";
+// import { HydrationBoundary, QueryClient, dehydrate } from "@tanstack/react-query";
+// import getServerAuthHeaders from "@/services/getServerAuthHeaders";
+// import { fetchAssignments } from "@/services/fetchApi";
 
 async function Layout({children}) {
-  const queryClient = new QueryClient();
-
-  await queryClient.prefetchQuery({
-    queryKey: ['assignments'],
-    queryFn: async ()=>{
-      const headers = await getServerAuthHeaders();
-      return await fetchAssignments(headers)
-    },
-  });
+  // const queryClient = new QueryClient();
+  //
+  // await queryClient.prefetchQuery({
+  //   queryKey: ['assignments'],
+  //   queryFn: async ()=>{
+  //     const headers = await getServerAuthHeaders();
+  //     return await fetchAssignments(headers)
+  //   },
+  // });
 
   return (
-    <HydrationBoundary state={dehydrate(queryClient)}>
-      {children}
-    </HydrationBoundary>
+    // <HydrationBoundary state={dehydrate(queryClient)}>
+      children
+    // </HydrationBoundary>
   );
 }
 
--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,9 +1,9 @@
 import "@/app/globals.css"
 import { Archivo_Black, Ubuntu } from "next/font/google";
 import Providers from "@/app/providers";
-import { dehydrate, HydrationBoundary, QueryClient } from "@tanstack/react-query";
-import getServerAuthHeaders from "@/services/getServerAuthHeaders";
-import { fetchAssignments, fetchUser } from "@/services/fetchApi";
+// import { dehydrate, HydrationBoundary, QueryClient } from "@tanstack/react-query";
+// import getServerAuthHeaders from "@/services/getServerAuthHeaders";
+// import { fetchAssignments, fetchUser } from "@/services/fetchApi";
 
 export const metadata = {
   title: "GradeIQ",
@@ -30,23 +30,23 @@ const archivo_black = Archivo_Black({
 })
 
 export default async function RootLayout({ children }) {
-  const queryClient = new QueryClient();
-
-  await queryClient.prefetchQuery({
-    queryKey: ['user'],
-    queryFn: async ()=>{
-      const headers = await getServerAuthHeaders();
-      return await fetchUser(headers)
-    },
-  });
+  // const queryClient = new QueryClient();
+  //
+  // await queryClient.prefetchQuery({
+  //   queryKey: ['user'],
+  //   queryFn: async ()=>{
+  //     const headers = await getServerAuthHeaders();
+  //     return await fetchUser(headers)
+  //   },
+  // });
 
   return (
     <html lang="en" suppressHydrationWarning className={`${ubuntu.variable} ${archivo_black.variable}`}>
       <body className="min-h-screen-dynamic flex flex-col">
         <Providers>
-          <HydrationBoundary state={dehydrate(queryClient)}>
+          {/*<HydrationBoundary state={dehydrate(queryClient)}>*/}
             {children}
-          </HydrationBoundary>
+          {/*</HydrationBoundary>*/}
         </Providers>
       </body>
     </html>
