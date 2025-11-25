4fbcd0ca6e8cc678699cbf165aecdd047bb3a087
Viktor Sharha <vsharha07@gmail.com>
2025-11-25 16:07:48 +0000

    update google provider name

--- src/components/custom/SelectModel.jsx
+++ src/components/custom/SelectModel.jsx
@@ -1,21 +1,21 @@
 import {
-  Select,
-  SelectContent,
-  SelectGroup,
-  SelectItem,
-  SelectLabel,
-  SelectTrigger,
-  SelectValue,
+    Select,
+    SelectContent,
+    SelectGroup,
+    SelectItem,
+    SelectLabel,
+    SelectTrigger,
+    SelectValue,
 } from "@/components/ui/select";
 import useModels from "@/hooks/useModels";
-import { useState } from "react";
-import { SiAnthropic, SiGoogle } from "react-icons/si";
+import {useState} from "react";
+import {SiAnthropic, SiGoogle} from "react-icons/si";
 import _ from "lodash";
-import { Brain } from "lucide-react";
+import {Brain} from "lucide-react";
 
 const providerIcons = {
   anthropic: <SiAnthropic />,
-  google: <SiGoogle />,
+  gemini: <SiGoogle />,
 };
 
 function SelectModel({ onChange, ...props }) {
